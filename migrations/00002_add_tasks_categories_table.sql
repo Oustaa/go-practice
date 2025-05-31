0dc1989b168090850d6e902546cd1742017e8a25
Oustaa <otailaba98@gmail.com>
2025-05-31 21:34:53 +0100

    completing the version one of migration

---
+++ migrations/00002_add_tasks_categories_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS tasks_categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE tasks_categories;
+-- +goose StatementEnd
--- migrations/00003_add_tasks_statuses_table.sql
+++ migrations/00003_add_tasks_statuses_table.sql
@@ -1,19 +1,16 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS tasks (
+CREATE TABLE IF NOT EXISTS tasks_statuses (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
-    description TEXT,
-    status_id INT NOT NULL, -- DONE, IN PROGRESS, TODO ( Defined later by users )
-    category_id INT NOT NULL, -- STUDY, WORK, PERSONAL ( Defined later by users )
+    name VARCHAR(50) NOT NULL UNIQUE,
+    category_id int NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (status_id) REFERENCES tasks_statuses (id),
     FOREIGN KEY (category_id) REFERENCES tasks_categories (id)
-)
+);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE tasks;
+DROP TABLE tasks_statuses;
 -- +goose StatementEnd
